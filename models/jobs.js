812aec4472fb2226ec99d06ef663a19f676f5a7a
Rich Benjamin <richmwendwa03@gmail.com>
2025-04-10 21:48:41 +0300

    Defined the structure of the data of how the posted jobs will be stored in the db with linkage to a user who applied and indexed for full text search

---
+++ models/jobs.js
@@ -0,0 +1,44 @@
+// models/jobs.js
+const mongoose = require("mongoose");
+
+const jobSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Please enter job title"],
+    trim: true
+  },
+  description: {
+    type: String,
+    required: [true, "Please enter job description"]
+  },
+  requirements: [String],
+  company: {
+    type: String,
+    required: [true, "Please enter company name"]
+  },
+  location: {
+    type: String,
+    required: [true, "Please enter job location"]
+  },
+  industry: String,
+  salaryRange: {
+    min: Number,
+    max: Number
+  },
+  postedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  applicants: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }]
+}, { 
+  timestamps: true,
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true } 
+});
+
+jobSchema.index({ title: "text", description: "text", location: "text" });
+module.exports = mongoose.model("Jobs", jobSchema);
